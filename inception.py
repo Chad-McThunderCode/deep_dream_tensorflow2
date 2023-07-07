1663a0d24ffdc8e6f7880966827de92e0961da94
Chad-McThunderCode <chadmcthundercode@protonmail.com>
2023-07-07 16:05:18 +0200

    Update inception.py
    aaaand another compatibility issue fixed: tf.io.gfile.GFile instead of tf.fastGFile

--- inception.py
+++ inception.py
@@ -12,7 +12,7 @@ class Inception:
     def __init__(self):
         self.graph = tf.Graph()
         with self.graph.as_default():
-            with tf.gfile.FastGFile("inception/5h/tensorflow_inception_graph.pb", "rb") as file:
+            with tf.io.gfile.GFile("inception/5h/tensorflow_inception_graph.pb", "rb") as file:
                 #graph_def = tf.GraphDef()
                 graph_def = tf.compat.v1.GraphDef()
                 graph_def.ParseFromString(file.read())
